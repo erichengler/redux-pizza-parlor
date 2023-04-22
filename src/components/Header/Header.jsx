cd0208dff5cab01df01a9846862a5c0730efe772
Erich Engler <erich.engler@hotmail.com>
2023-04-22 14:45:30 -0500

    clicking on the header title now leads the user back to home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,26 @@
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import './Header.css';
 
 function Header () {
 
     const cartTotal = useSelector(store => store.cartTotal);
 
+    const history = useHistory();
+
+    const backToHome = () => {
+        history.push('/');
+    }
+
     return(
         <>
             <header className='App-header'>
-                <h1 className='App-title'>Prime Pizza</h1>
-                <div className='cart-total'>Total: ${cartTotal.toFixed(2)}</div>
+                <h1 className='App-title' onClick={backToHome}>
+                        Prime Pizza
+                </h1>
+                <div className='cart-total'>
+                    Total: ${cartTotal.toFixed(2)}
+                </div>
             </header>
         </>
     )
